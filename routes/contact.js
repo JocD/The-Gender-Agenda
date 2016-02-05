6fd02bd9594da0e5343cc25248b652edd35e78e9
Jacques Dukes <jdukes22@gmail.com>
2016-02-05 12:01:08 +1100

    Added nodemailer support. Still need to sanitize input and use AJAX rather than reloading the entire page. Also apply email template

---
+++ routes/contact.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+var mg = require('nodemailer-mailgun-transport');
+var recipient = 'jdukes22@gmail.com';
+
+var auth = {
+    auth: {
+        api_key: 'key-39931881223cd1a91919357529a7ebfb',
+        domain: 'mg.jacquesdukes.com'
+    }
+};
+
+var nodemailerMailgun = nodemailer.createTransport(mg(auth));
+
+/* GET users listing. */
+router.post('/', function (req, res, next) {
+    var formData = req.body;
+    nodemailerMailgun.sendMail({
+            from: formData.name + ' <' + formData.email + '>',
+            to: recipient, // An array if you have multiple recipients.
+            subject: formData.subject,
+            text: formData.message
+        })
+        .then(function (info) {
+            res.status(200).end();
+        })
+        .catch(function (err) {
+            console.log("Error sending email: " + err);
+            res.status(500).end();
+        });
+});
+
+module.exports = router;
