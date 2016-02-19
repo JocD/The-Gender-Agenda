efa5287e2d1d380b56189a999bb8fd5a89767328
Jacques Dukes <jdukes22@gmail.com>
2016-02-19 11:21:55 +1100

    Changed config.js to point to new remote Wordpress mount

--- routes/config.js
+++ routes/config.js
@@ -6,7 +6,7 @@ var Config = {
     },
     prd: {
         contact: 'host@thegenderagenda.com',
-        server: '128.199.152.181/html',
+        server: '128.199.152.181/wordpress',
         applicationPassword: 'OJZJ f6vU mQeq pgRu'
     },
 
